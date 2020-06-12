d3548bc04c8d39788354e6cdcfa436b09033f521
DimPap99 <dimtrispap1999@gmail.com>
2020-06-12 18:29:53 +0300

    added more scores, better diagrams

--- MLPpartA.py
+++ MLPpartA.py
@@ -9,7 +9,7 @@ from keras.layers import Dense
 from keras.layers import SimpleRNN, LSTM, GRU
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
 from sklearn.preprocessing import MinMaxScaler
 
 
@@ -37,6 +37,7 @@ data = pd.read_excel(file_name, header=None, skiprows=2)
 data.dropna(inplace=True)
 data = data.iloc[0:530]
 scaler = MinMaxScaler(feature_range=(-1, 1))
+feature_scaler = MinMaxScaler(feature_range=(-1, 1))
 
 stock = data[2].to_frame(name='USD ISE')
 stock_df = scaler.fit_transform(np.reshape(stock['USD ISE'].values, (stock.shape[0], 1)))
@@ -50,21 +51,15 @@ del features['date']
 del features['TL ISE']
 del features['USD ISE']
 
-n_in = 3
+scaled_features = feature_scaler.fit_transform(np.reshape(features.values, (features.shape[0], features.shape[1])))
+scaled_features = pd.DataFrame(data=scaled_features, columns=features.columns)
+
+n_in = 8
 n_out = 1
-superv_features = timeseries_to_supervised(features, n_in, n_out)
+superv_features = timeseries_to_supervised(scaled_features, n_in, n_out)
 superv_ise = timeseries_to_supervised(stock_df, n_in, n_out)
 temp = superv_ise.copy()
 temp.drop(columns=['USD ISE(t)'])
-#del superv_features['SP(t)']
-#del superv_features['DAX(t)']
-#del superv_features['FTSE(t)']
-#del superv_features['NIKEEI(t)']
-#del superv_features['BOVESPA(t)']
-#del superv_features['EU(t)']
-#del superv_features['EM(t)']
-
-#superv_features = pd.concat([superv_features, temp.drop(columns=['USD ISE(t)'])], axis=1)
 print(superv_ise)
 X, y =  superv_features.values, superv_ise['USD ISE(t)'].values
 
@@ -75,9 +70,9 @@ print(y)
 train_set_sz = round(X.shape[0] * 0.6)
 # test set size will be the remaining samples
 
-features_in = n_in
 features_out = n_out
 print(train_set_sz)
+
 #For X/y training np array take the rows from 0 index up to train_set_sz index
 #For X,y test np arrays take the rows remaining in the dataset. From train_set_sz index til the end.
 Xtr, ytr = X[:train_set_sz, :], y[:train_set_sz]
@@ -85,6 +80,14 @@ Xte, yte = X[train_set_sz:, :], y[train_set_sz:]
 
 batch_size = 5
 model = Sequential()
+
+print("n_in: ", n_in)
+print("n_out", n_out)
+print("features_out" , features_out)
+print("Xtr dimentions: ", Xtr.shape)
+print("Batch size: ", batch_size)
+print("Trainset size: " , train_set_sz)
+print("Test set size: " ,Xte.shape[0] )
 print(Xtr.shape[1])
 model.add(Dense(units=50, input_dim=Xtr.shape[1], activation="relu"))
 model.add(Dense(50, activation="relu"))
@@ -98,6 +101,7 @@ trainSetPrediction = model.predict(Xtr, batch_size=batch_size)
 testSetPrediction = model.predict(Xte, batch_size=batch_size)
 ytr2d = np.reshape(ytr, (ytr.shape[0], features_out))
 yte2d = np.reshape(yte, (yte.shape[0], features_out))
+print(ytr2d.shape)
 
 trainSetPrediction = scaler.inverse_transform(trainSetPrediction)
 trainY = scaler.inverse_transform(ytr2d)
@@ -107,6 +111,7 @@ testY = scaler.inverse_transform(yte2d)
 # calculate error
 print("Test MSE: ", mean_squared_error(testY, testSetPrediction))
 print("Test MSE: ", sum(np.square(testY-testSetPrediction))/testY.shape[0])
+print('Test Explained Variance score: ', explained_variance_score(testY, testSetPrediction))
 print("Test MAE: ", sum(abs(testY-testSetPrediction))/testY.shape[0])
 print("Test R2: ", r2_score(testY, testSetPrediction))
 print("Test R2: ", 1-(sum(np.square(testY-testSetPrediction))/sum(np.square(testY-testY.mean()))))
--- RNNpartb.py
+++ RNNpartb.py
@@ -8,11 +8,10 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import SimpleRNN, LSTM, GRU
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, explained_variance_score
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import MinMaxScaler
 from preprocessing import preprocess
-
 
 file_name = 'data_akbilgic.xlsx'
 data = pd.read_excel(file_name, header=None, skiprows=2)
@@ -21,7 +20,9 @@ data = data.iloc[0:530]
 data.columns = ['date', 'TL ISE', 'USD ISE', 'SP', 'DAX', 'FTSE', 'NIKEEI', 'BOVESPA', 'EU', 'EM']
 #print(data)
 rnn_input = list()
-superv_stock, superv_features, rnn_input, index_scaler, feature_scaler = preprocess(data, 7, 1, False)
+n_in = 2
+n_out = 1
+superv_stock, superv_features, rnn_input, index_scaler, feature_scaler = preprocess(data, n_in, n_out, True)
 
 total_data_num = rnn_input[0]
 
@@ -33,7 +34,7 @@ test_size = rnn_input[0] - train_size
 
 print ("Train size: %d" % train_size)
 print ("Test size: %d" % (rnn_input[0] - train_size))
-
+print("Samples: " , rnn_input[0])
 xtr, ytr = X[:train_size], y[:train_size]
 xte, yte = X[train_size:], y[train_size:]
 print(xtr.shape, ytr.shape)
@@ -41,19 +42,25 @@ print(xte.shape, yte.shape)
 
 training_samples = train_size
 test_samples = test_size
-steps = rnn_input[1]
+steps = 1
 
 features_in = rnn_input[2]
 features_out = 1
 xtr = np.reshape(xtr, (training_samples, steps, features_in))
 ytr = np.reshape(ytr, (training_samples, steps, features_out))
 
-print(xtr.shape, ytr.shape)
+print('xtr vector dimentions: ', xtr.shape, ytr.shape)
 xte = np.reshape(xte, (test_samples, steps, features_in))
 yte = np.reshape(yte, (test_samples, steps, features_out))
-print(xte.shape, yte.shape)
+print('xte vector dimentions: ', xte.shape, yte.shape)
 
-
+print(" Test samples: ", test_samples)
+print(" Training samples: ", training_samples)
+print("Steps: ", steps)
+print("Features In: ", features_in)
+print("Features Out: ", features_out)
+print("n_in: ",n_in )
+print("n_out: ", n_out)
 batch_size = 1
 model = Sequential()
 
@@ -67,31 +74,28 @@ model.fit(xtr,ytr, epochs=10, batch_size=batch_size, verbose=0)
 
 trainPredict = model.predict(xtr, batch_size=batch_size)
 testPredict = model.predict(xte, batch_size=batch_size)
-
 # invert predictions
 trainPredict = np.reshape(trainPredict, (training_samples*steps, features_out))
 ytr2d = np.reshape(ytr, (training_samples*steps, features_out))
 testPredict = np.reshape(testPredict, (test_samples*steps, features_out))
 yte2d = np.reshape(yte, (test_samples*steps, features_out))
 #
-print(trainPredict)
 trainPredict = index_scaler.inverse_transform(trainPredict)
 trainY = index_scaler.inverse_transform(ytr2d)
 testPredict = index_scaler.inverse_transform(testPredict)
 testY = index_scaler.inverse_transform(yte2d)
-
 # calculate error
 print("Test MSE: ", mean_squared_error(testY, testPredict))
 print("Test MSE: ", sum(np.square(testY-testPredict))/testY.shape[0])
 print("Test MAE: ", sum(abs(testY-testPredict))/testY.shape[0])
+print('Test Explained Variance score: ', explained_variance_score(testY, testPredict))
+
 print("Test R2: ", r2_score(testY, testPredict))
 print("Test R2: ", 1-(sum(np.square(testY-testPredict))/sum(np.square(testY-testY.mean()))))
-
 # Finally, we check the result in a plot.
 # A vertical line in a plot identifies a splitting point between
 # the training and the test part.
 predicted = np.concatenate((trainPredict,testPredict),axis=0)
-
 original = np.concatenate((trainY,testY),axis=0)
 predicted = np.concatenate((trainPredict,testPredict),axis=0)
 index = range(0, original.shape[0])
